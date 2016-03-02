7d9dd66a011b9b5a4d94a54f023775624b65d506
luchanso <oleg@loutchansky.com>
2016-03-03 01:40:33 +0300

    Fix

--- library.js
+++ library.js
@@ -5,25 +5,6 @@
     winston = module.parent.require('winston'),
     User = require.main.require('./src/user');
 
-  Fix.firstRun = function(params, callback) {
-    winston.info("[nodebb-plugin-grab-fix] run");
-
-    User.getUsersFromSet('users:joindate', 1, 1, 200, function(err, users) {
-      if (err) {
-        winston.error(err);
-        callback();
-        return;
-      }
-
-      users.forEach(function(user) {
-        winston.info("[nodebb-plugin-grab-fix] Fix: " + user.uid);
-        User.ignoreCategory(user.uid, 21, function() {});
-      });
-    });
-
-    callback();
-  }
-
   Fix.userCreate = function(userData) {
     User.ignoreCategory(userData.uid, 21, function() {});
   }
