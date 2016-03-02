179e96de0e89b1c8cad7072c53d8aca19649618d
luchanso <oleg@loutchansky.com>
2016-03-03 01:25:57 +0300

    Fix

--- library.js
+++ library.js
@@ -2,7 +2,19 @@
   "use strict";
 
   var Fix = {},
+    winston = module.parent.require('winston'),
     User = require.main.require('./src/user');
+
+  Fix.firstRun = function() {
+    User.getUsersFromSet('users:joindate', 1, 0, 200, function(err, result) {
+      winston.info("[nodebb-plugin-grab-fix] Found users: " + result.users.length);
+
+      result.users.forEach(function(user) {
+        winston.info("[nodebb-plugin-grab-fix] Fix: " + user.uid);
+        User.ignoreCategory(user.uid, 21, function() {});
+      });
+    });
+  }
 
   Fix.userCreate = function(userData) {
     User.ignoreCategory(userData.uid, 21, function() {});
