d922b73d230f8d541de30fd627ccb1bef91598e2
luchanso <oleg@loutchansky.com>
2016-03-03 01:38:59 +0300

    Fix

--- library.js
+++ library.js
@@ -8,14 +8,14 @@
   Fix.firstRun = function(params, callback) {
     winston.info("[nodebb-plugin-grab-fix] run");
 
-    User.getUsersFromSet('users:joindate', 1, 1, 200, function(err, result) {
+    User.getUsersFromSet('users:joindate', 1, 1, 200, function(err, users) {
       if (err) {
         winston.error(err);
         callback();
         return;
       }
 
-      result.users.forEach(function(user) {
+      users.forEach(function(user) {
         winston.info("[nodebb-plugin-grab-fix] Fix: " + user.uid);
         User.ignoreCategory(user.uid, 21, function() {});
       });
